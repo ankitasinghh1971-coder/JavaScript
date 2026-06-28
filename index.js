4c83c88fdfe5558d99d51368fc61a263cef5ccf8
ankitasinghh1971-coder <ankitasinghh1971@gmail.com>
2026-06-29 02:51:00 +0530

    datatypes ans ECMA standards

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 console.log("Ankita");
+
+
+/**
+ * var → old way, function-scoped, can redeclare and reassign, avoid using it.
+let → block-scoped, can reassign but cannot redeclare in the same scope.
+const → block-scoped, cannot reassign or redeclare, best choice by default.
+ */
+//if you leave a variable undeclared it will be undefined
+const accoundId = "Ankita";
+let accountEmail = "ank257z@gmail.com";
+var accountPassword = "12345"
+
+
+console.table([accoundId,accountEmail,accountPassword,accountCity]);
---
+++ lecture2.js
@@ -0,0 +1,31 @@
+"use strict" // trate all JS code as newer version
+
+//alert("Hello") we are using Node.js or browser
+// console.log(3+3) console.log(); this will give error 
+ // code readibilty should be high
+
+
+ let name = "Ankta"
+ let age = 18
+ let isLoggedIn = false
+
+ //datatypes
+ /**
+  * number
+  * bigint
+  * string
+  * boolean
+  * null => standalone value let state = null
+  * undefined => let state; 
+  * symbol => unique
+  * 
+  * object
+  */
+ console.log(typeof(name));
+ console.log(typeof(null));//Object
+ console.log(typeof(undefined));//undefined
+
+ 
+ 
+ 
+ 
